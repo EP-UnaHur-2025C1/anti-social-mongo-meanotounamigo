6e565c907d85bb7340016277700856af90883cb6
Loly Denhoff <lorenadenhoff@gmail.com>
2025-06-15 00:54:01 -0300

    Configuración de archivo db.js y main.ja

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express = require ('express')
+const conectarDB = require('./db/db')
 
 
 const app = express() 
 const PORT = process.env.PORT || 3000
 
+//Conexion a MongoDB
+conectarDB()
+
 app.listen(PORT, () =>{
-    console.log ('Servidor escuchando en el puerto ${PORT}')
+    console.log ('Servidor escuchando en el puerto http://localhost:${PORT}')
 })
