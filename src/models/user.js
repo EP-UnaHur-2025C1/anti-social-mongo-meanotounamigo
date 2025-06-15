ac36fd30c3a3d395ec570d6feda41219e3c5e059
Loly Denhoff <lorenadenhoff@gmail.com>
2025-06-15 01:48:12 -0300

    Models:user

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    nickname:{
+        type: String, 
+        required: true, 
+        unique:true, 
+        minlength:[3,'El nombre debe tener al menos 3 letras'], 
+        required:[true, 'El nombre es obligatorio']
+    },
+    email:{
+        type: String, 
+        required: true, 
+        unique:true, 
+        required:[true, 'El mail es obligatorio']
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
