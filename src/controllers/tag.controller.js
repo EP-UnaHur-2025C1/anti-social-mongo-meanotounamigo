375df1b9635eba626973669ebc3f1f11908c3c54
Squarejuggernaut <facundo_leonel_paz63@hotmail.com.ar>
2025-06-21 15:57:32 +0000

    fix: hice los controladores que creo justos y necesarios de momento con una lógica más limpia y entendible

--- src/controllers/tag.controller.js
+++ src/controllers/tag.controller.js
@@ -1,41 +1,37 @@
 const  { Tag, Post } = require('../mongoSchemas')
 
 const getTags = async ( _ , res) =>{
-    const data = await Tag.find({});
-    res.status(200).json(data);
+    const tags = await Tag.find({});
+    res.status(200).json(tags);
 };
 
 const getTagById = async (req, res) =>{
-    const data = await Tag.findById(req.params.id);
-    res.status(200).json(data);
+    const tag = req.tag;
+    res.status(200).json(tag);
 };
 
 const createTag = async (req, res) =>{
-    try{
-        const newTag = new Tag(req.body);
-        await newTag.save();
-        res.status(201).json(newTag);
-    } catch (err){
-        res.status(400).json({error: err.message});
-    }
+    const newTag = new Tag(req.body);
+    await newTag.save();
+    res.status(201).json(newTag);
 };
 
 const updateTagById = async (req, res) =>{
     const {nombre} = req.body;
-    const tag = await Tag.findById(req.params.id);
+    const tag = req.tag;
     tag.nombre = nombre;
     await tag.save();
-    res.status(200).json({message: 'Tag actualizado correctamente', tag});
+    res.status(200).json({message: 'Etiqueta actualizada correctamente', tag});
 };
 
 const deleteTagById = async (req, res) => {
-  const tag = await Tag.findByIdAndDelete(req.params.id);
-  res.status(200).json({ message: 'Tag eliminada correctamente', tag });
+  await req.tag.deleteOne();
+  res.status(200).json({ message: "Etiqueta eliminada correctamente" });
 };
 
 const getPostsByTagId = async (req, res) => {
-  const { tagId } = req.params;
-  const posts = await Post.find({ etiquetas: tagId });
+  const tag = req.tag;
+  const posts = await Post.find({ etiquetas: tag._id });
   res.status(200).json(posts);
 };
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,39 +1,34 @@
 const { User } = require('../mongoSchemas')
 
-//CRUD básicos
-
+//CRUD
 const getUsers = async ( _ , res) =>{
-  const data = await User.find({});
-  res.status(200).json(data);
+  const users = await User.find({});
+  res.status(200).json(users);
 };
 
 const getUserById = async (req, res) => {
-    const data = await User.findById(req.params.id);
-    res.status(200).json(data);
+    const user = req.user;
+    res.status(200).json(user);
 };
 
 const createUser = async (req, res) => {
-    try{
-        const newUser = new User(req.body);
-        await newUser.save();
-        res.status(201).json(newUser);
-    } catch (err){
-        res.status(400).json({error: err.message});
-    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
 };
 
 const updateUserById = async (req, res) => {
     const{nickname, email} = req.body;
-    const user = await User.findById(req.params.id);
+    const user = req.user;
     user.nickname = nickname;
     user.email = email;
     await user.save();
     res.status(200).json({message: 'Usuario actualizado correctamente', user});
 };
 
-const deleteUserById = async (req, res) =>{
-    const user = await User.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: 'Usuario eliminado correctamente', user})
+const deleteUserById = async (req, res) => {
+  await req.user.deleteOne();
+  res.status(200).json({ message: 'Usuario eliminado correctamente', user: req.user });
 };
 
 module.exports = {
@@ -42,13 +37,4 @@ module.exports = {
     createUser, 
     updateUserById, 
     deleteUserById,
-    //userCreatePost, 
-    //getUserByIdWithPosts, 
-    //getUserByIdWithAPost,
-    //userCreateComment, 
-    //userDeletePostById, 
-    //userUpdatePost, 
-    //userDeleteCommentById,         
-    //userUpdateComment, 
-    //userGetComment
 }
