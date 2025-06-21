a87298e704058490dd84a87396fea52c86d0a23b
Squarejuggernaut <facundo_leonel_paz63@hotmail.com.ar>
2025-06-21 15:41:31 +0000

    feat: necesariamente separé los joi de creacion con los de actualizacion, un joi para la asignacion de tags que controla que no se repitan las tags o que existan y esta todo centralizado en el index

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,15 +1,19 @@
 const postSchema = require("./post.create.schema");
 const postUpdateSchema = require("./post.update.schema")
+const commentCreateSchema = require("./comment.create.schema")
+const commentUpdateSchema = require("./comment.update.schema")
 const postImageSchema = require("./postImageSchema");
 const userSchema = require("./user.schema");
-const commentSchema = require("./comment.schema");
 const tagSchema = require("./tags.schema");
+const tagAssignmentSchema = require("./tagAssigment.schema");
 
 module.exports = {
     postSchema, 
     postUpdateSchema,
     postImageSchema, 
     userSchema, 
-    commentSchema, 
-    tagSchema
+    commentCreateSchema,
+    commentUpdateSchema,
+    tagSchema,
+    tagAssignmentSchema
 };
