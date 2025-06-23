0b7d85697c88b73fa4ede3e64df5cbebd4fde4d4
Squarejuggernaut <facundo_leonel_paz63@hotmail.com.ar>
2025-06-23 04:46:36 +0000

    fix: variables de entorno y conexión con mongo manejadas de una manera más coherente, segura y ordenada

--- src/bd/mongodb.js
+++ src/bd/mongodb.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
-const MONGO_URL = process.env.MONGO_URL ?? "mongodb://root:example@mongo:27017/redSocial?authSource=admin";
+
+//Controla que ninguna de las 3 variables de entorno en el .env lleguen vacías
+['MONGO_ROOT_USERNAME', 'MONGO_ROOT_PASSWORD', 'MONGO_DB_NAME'].forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Falta la variable de entorno: ${key}`);
+  }
+});
+
+//Construí la URI interpolando partes del .env no lo pude construir allí porque .env no reconoce el símbolo "?"
+const MONGO_URI = `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongo:27017/${process.env.MONGO_DB_NAME}?authSource=admin`;
 
 let isConnected;
 
-const connectToDataBase = async() =>{
-    if(!isConnected){
-      await mongoose.connect(MONGO_URL);
-      console.log("Conexión con Mongo realizada con éxito"); 
-      isConnected = true;  
-    }
+const connectToDataBase = async () => {
+  if (!isConnected) {
+    await mongoose.connect(MONGO_URI);
+    console.log("Conexión con Mongo realizada con éxito");
+    isConnected = true;
+  }
 };
- 
-module.exports = {mongoose, connectToDataBase};
+
+module.exports = { mongoose, connectToDataBase };
